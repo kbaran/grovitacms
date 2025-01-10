481c008ca3de7a2c71ef390f6d18c5ed4c5d0f33
bmkaradia <bmk302006@gmail.com>
2025-01-10 14:17:40 +0530

    fixed institute condition for coursecategory

--- src/access/isAdminOrManager.tsx
+++ src/access/isAdminOrManager.tsx
@@ -1,31 +1,33 @@
-import { Access } from 'payload';
+import { Access } from 'payload'
 
 export const isAdminOrManager: Access = ({ req: { user } }) => {
   if (!user) {
-    return false; // Deny access if no user is logged in
+    return false // Deny access if no user is logged in
   }
 
-  const { role, instituteId } = user;
+  const { role, instituteId } = user
 
   // Full access for admin
   if (role === 'admin') {
-    return true; // Allow admin full access
+    return true // Allow admin full access
   }
 
   // Restrict account managers to their own institute's data
   if (role === 'accountmanager') {
-    if (!instituteId) {
+    //console.log('🚀 Brij  ~  file: isAdminOrManager.tsx:18 ~  o:', user)
+
+    if (!instituteId?.id) {
       // If instituteId is missing, deny access
-      return false;
+      return false
     }
 
     return {
       instituteId: {
-        equals: instituteId, // Match the Object ID directly
+        equals: instituteId?.id, // Match the Object ID directly
       },
-    };
+    }
   }
 
   // Deny access for all other roles
-  return false;
-};
+  return false
+}

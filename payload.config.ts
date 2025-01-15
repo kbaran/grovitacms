1ef724d142ac2b2a62c955cb915243cf23e16b02
kbaran <99946200+Ayanonize@users.noreply.github.com>
2025-01-15 18:33:34 +0530

    Update payload.config.ts

--- payload.config.ts
+++ payload.config.ts
@@ -57,6 +57,7 @@ const mediaField = {
 // const imagekit = new ImageKit({
 //   publicKey: process.env.IMAGEKIT_PUBLIC_KEY!,
 //   privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+
 //   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT!,
 // })
 
